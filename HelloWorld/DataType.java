bb00856f06da8d3b39e4b45bd29f8b1e772db2fb
Priyanka Takale <85220239+Priyanka2101-02@users.noreply.github.com>
2023-08-11 22:37:05 +0530

    Add files via upload

---
+++ HelloWorld/DataType.java
@@ -0,0 +1,28 @@
+package myPackage;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// Primitive  Data Types
+		byte num1 = 5; //8 bit
+		short num2= 125; // 2 byte
+		int  num3 = 578694; // 4byte
+		long num4 = 45896123; // 8 byte
+		float num5 = 29.21f; // 4 byte
+		double num6 = 123.569; //8 byte
+		char st = 'P'; 
+		boolean flag = true; // 1 bit 
+		
+		
+		System.out.println("The value of Byte is " +num1);
+		System.out.println("The value of short is " +num2);
+		System.out.println("The value Integer is " +num3);
+		System.out.println("The value of Long is " +num4);
+		System.out.println("The value of Float is " +num5);
+		System.out.println("The value of Double is " +num6);
+		System.out.println("The value of Character is " +st);
+		System.out.println("The value of Boolean is " +flag);
+		
+	}
+
+}
---
+++ HelloWorld/Helloworld.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+
+	}
+	// there are n no of the classes jvm check for main method it check main mehtods class name as file namae 
+	// can we run java prg without main methods
+	//--> before java7 it is possible but after java7 it is not possible
+
+}
