de2defe7786c440d0c2ed23f9dd040074b84975a
Priyanka Takale <85220239+Priyanka2101-02@users.noreply.github.com>
2023-08-24 16:03:05 +0530

    Add files via upload

---
+++ AddWhiteSpace/Str.java
@@ -0,0 +1,20 @@
+package addWhiteSpace;
+
+public class Str {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str ="Edubridge";
+		//System.out.println(str.length());
+		
+		char[] ch = str.toCharArray();
+		//System.out.println(ch.length);
+		for(int i=0;i<ch.length;i++)
+		{
+			System.out.print(ch[i]+" ");
+		}
+		
+
+	}
+
+}
